46c2722f7e056829aaeb7a5f6ab7460738ce0602
mojojompe <157244574+mojojompe@users.noreply.github.com>
2025-09-12 09:59:32 +0100

    Hmm

--- ecommerce-backend/src/middlewares/adminMiddleware.js
+++ ecommerce-backend/src/middlewares/adminMiddleware.js
@@ -1,16 +1,21 @@
-const adminMiddleware = (req, res, next) => {
-    // Check if the user is authenticated
-    if (!req.user) {
-        return res.status(401).json({ message: 'Unauthorized access. Please log in.' });
-    }
+exports.adminMiddleware = (req, res, next) => {
+  // Check if the user is authenticated
+  if (!req.user) {
+    return res
+      .status(401)
+      .json({ message: "Unauthorized access. Please log in." });
+  }
 
-    // Check if the user has admin privileges
-    if (req.user.role !== 'admin') {
-        return res.status(403).json({ message: 'Forbidden. You do not have permission to access this resource.' });
-    }
+  // Check if the user has admin privileges
+  if (req.user.role !== "admin") {
+    return res
+      .status(403)
+      .json({
+        message:
+          "Forbidden. You do not have permission to access this resource.",
+      });
+  }
 
-    // If the user is authenticated and is an admin, proceed to the next middleware or route handler
-    next();
+  // If the user is authenticated and is an admin, proceed to the next middleware or route handler
+  next();
 };
-
-module.exports = adminMiddleware;
