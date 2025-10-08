f86f3b5beca5a16c3bfc798dae3454c527668575
mojojompe <157244574+mojojompe@users.noreply.github.com>
2025-10-08 12:50:48 +0100

    Finals

--- ecommerce-backend/src/app.js
+++ ecommerce-backend/src/app.js
@@ -29,6 +29,15 @@ app.use("/api/admin", authMiddleware, adminRoutes);
 
 app.use(errorHandler);
 
+
+import mongoose from "mongoose";
+
+const uri = process.env.DATABASE_URL;
+
+mongoose.connect(uri)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.error("MongoDB connection error:", err));
+
 const PORT = process.env.PORT || 5000;
 connectDB();
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
